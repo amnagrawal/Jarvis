6bcd9f70f363843e97726f465dd1a0c6fe9e7956
Armaan <armaankohli1995@yahoo.co.in>
2016-02-10 15:08:33 +0530

    UI prepared

--- app/src/main/java/com/aman/jarvis/MainActivity.java
+++ app/src/main/java/com/aman/jarvis/MainActivity.java
@@ -2,12 +2,31 @@ package com.aman.jarvis;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button button;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = (EditText)findViewById(R.id.editText);
+        button = (Button)findViewById(R.id.button);
+        textView = (TextView)findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                textView.setText("Tera katta hone wala hai");
+            }
+        });
     }
 }
+
